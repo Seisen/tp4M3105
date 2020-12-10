829f8f27e894e948eb56fe3b68872b015b4a28e6
seisen <jochim6.2@hotmail.fr>
2020-12-10 18:05:57 +0100

    introduction

---
+++ src/PremiereFenetre.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class PremiereFenetre {
+    public static void main ( String args []){
+        JFrame fen = new JFrame () ;
+        fen.setSize (300 , 150) ;
+        fen.setTitle ("Ma premiere fenetre ") ;
+        fen.setVisible ( true ) ;
+    }
+}
